dcaac700164420d2039bc86aeb93d06045a2b836
karthik.ramnath.b <torqsama1993@gmail.com>
2021-04-11 14:30:16 +0530

    authentication logic added

--- xyzbanksvc/src/main/java/com/xyzbanksvc/service/LoginService.java
+++ xyzbanksvc/src/main/java/com/xyzbanksvc/service/LoginService.java
@@ -1,9 +1,15 @@
 package com.xyzbanksvc.service;
 
+import java.util.Optional;
+
 import com.xyzbanksvc.constants.ServiceConstants;
 import com.xyzbanksvc.model.ResponseStatus;
 import com.xyzbanksvc.model.User;
+import com.xyzbanksvc.repository.UserRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,17 +18,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginService {
 
+    @Autowired
+    UserRepository userRepo;
+
     public ResponseStatus authenticateUser(User user)
     {
         String status = "";
         ResponseStatus responseStatus = new ResponseStatus();
         responseStatus.setStatusCode(ServiceConstants.FAILURE_STATUS_CODE);
         responseStatus.setMessage(ServiceConstants.AUTH_FAILURE_MESSAGE);
-
-        if(status.equals(ServiceConstants.SUCCESS_STATUS_CODE))
+        Optional<User> fetchedUser = userRepo.findById(user.getUserId());
+        if(fetchedUser.isPresent())
         {
-            responseStatus.setStatusCode(ServiceConstants.SUCCESS_STATUS_CODE);
-            responseStatus.setMessage(ServiceConstants.AUTH_SUCCESS_MESSAGE);
+            PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+            String encodedPassword = encoder.encode(user.getPassword());
+            if(fetchedUser.get().getPassword().equals(encodedPassword))
+            {
+                responseStatus.setStatusCode(ServiceConstants.SUCCESS_STATUS_CODE);
+                responseStatus.setMessage(ServiceConstants.AUTH_SUCCESS_MESSAGE);
+            }
         }
         return responseStatus;
     }
