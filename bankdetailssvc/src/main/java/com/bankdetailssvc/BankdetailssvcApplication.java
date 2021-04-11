ebf6617497876a95d1fe1444c6300963fa19137b
91750 <91750@DESKTOP-AKMF731>
2021-04-11 11:49:52 +0530

    skeleton for bank details service

---
+++ bankdetailssvc/src/main/java/com/bankdetailssvc/BankdetailssvcApplication.java
@@ -0,0 +1,13 @@
+package com.bankdetailssvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankdetailssvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankdetailssvcApplication.class, args);
+	}
+
+}
---
+++ bankdetailssvc/src/test/java/com/bankdetailssvc/BankdetailssvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankdetailssvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankdetailssvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
