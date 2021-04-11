f50011d63ec4cc2f330921affc68cd924e895be5
91750 <91750@DESKTOP-AKMF731>
2021-04-11 11:43:30 +0530

    skeleton

---
+++ xyzbanksvc/src/main/java/com/xyzbanksvc/XyzbanksvcApplication.java
@@ -0,0 +1,13 @@
+package com.xyzbanksvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzbanksvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzbanksvcApplication.class, args);
+	}
+
+}
